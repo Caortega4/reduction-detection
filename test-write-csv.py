ba53d49f349a3891e5e611b4aaca370ec5c9cc49
Carlos Ortega <caortega4@miners.utep.edu.com>
2023-03-12 14:01:12 -0600

    Clear outputs

--- test-write-csv.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-annotation = [0, 1, 2]
-
-with open('metadata.csv', 'w', newline='') as f:
-    writer = csv.writer(f)
-    id = 234
-    writer.writerow([annotation[0], 'Profession'])
-    writer.writerow([annotation[0], 'Profession'])
